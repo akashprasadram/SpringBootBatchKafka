7a7bf9151d4f6a41575bed88a45e71e5bc314567
akashprasadram <akashprasadram99@gmail.com>
2024-01-27 03:05:56 +0530

    Adding files

---
+++ src/main/java/com/mhv/batchprocessing/BatchProcessingApplication.java
@@ -0,0 +1,15 @@
+package com.mhv.batchprocessing;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:kafka.properties")
+public class BatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessingApplication.class, args);
+	}
+}
